4567cbf68a003e3605e510ffdc7f894bbe74776d
NAm <nma@nma.local>
2022-02-15 15:02:01 +0700

    upload 2

--- user.sh
+++ user.sh
@@ -37,24 +37,40 @@ deluser(){
 addgroups(){
 	if [ $(id -u) -eq 0 ]; then
 	read -p "Enter groupname : " groupname
-	getent passwd $username
+	egrep "^$groupname" /etc/group >/dev/null
 	if [ $? -eq 0 ]; then
-		userdel -r "$username"
-		echo "User "$username" deleted"
+		echo "$groupname exists!"
+		exit
 	else
-		echo "$username not exists!"
-		exit 1
+		groupadd "$groupname"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit
+fi
+}
+delgroup(){
+	if [ $(id -u) -eq 0 ]; then
+	read -p "Enter groupname : " groupname
+	egrep "^$groupname" /etc/group >/dev/null
+	if [ $? -eq 0 ]; then
+		groupdel "$groupname"
+		echo "Group "$groupname" deleted"
+	else
+		echo "$groupname not exists!"
+		exit 
 	fi
 	else
 		echo "Only root may add a user to the system."
-		exit 2
+		exit
 	fi
 }
 
-
 adduser
-#addgroups
+addgroups
 deluser
-#delgroup
+delgroup
 #usermod
 #gpasswd
+#menu
